d2c55585fb39b14fb07e603faa94137384e84a2d
Turan <90398677+Turan141@users.noreply.github.com>
2023-12-13 19:23:03 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,63 @@
+const CACHE_NAME = 'your-app-cache-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/icons/app-icon-180x180.png',
+  '/icons/app-icon-192x192.png',
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('cache-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './manifest.json',
-      ]);
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        try {
+          return cache.addAll(urlsToCache);
+        } catch (error) {
+          console.error('Caching failed:', error);
+        }
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('your-app-cache-') && cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
   );
 });
